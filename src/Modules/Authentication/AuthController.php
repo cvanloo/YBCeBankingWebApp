d6b4751e94fb3c413b2401fb5aaebc2a5687e13c
Colin van Loo <colin.vanloo@edu.tbz.ch>
2021-10-01 11:31:51 +0200

    Begun implementing AuthController.

---
+++ src/Modules/Authentication/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+namespace Modules\Authentication;
+
+use Modules\Entity\User;
+
+public static class AuthController {
+    public static register(User $user) : User {
+
+    }
+
+    public static login(string $username, string $password) : User {
+
+    }
+
+    public static logout(User $user) : User {
+
+    }
+}
+
+?>
